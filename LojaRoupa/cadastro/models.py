67bbb03674004c858eeb58907bda3bb9f6a9b52a
Bruno Paluco <brunopaluco@gmail.com>
2023-05-27 10:37:21 -0300

    Correção de classes

--- LojaRoupa/cadastro/models.py
+++ LojaRoupa/cadastro/models.py
@@ -6,31 +6,29 @@ class Cliente(models.Model):
     nome = models.CharField(max_length=28, verbose_name="Nome")
     cpf = models.CharField(max_length=11, verbose_name="CPF")
     rg = models.CharField(max_length=10, verbose_name="RG")
-    birth_date = models.DateField()
+    data_de_nascimento = models.DateField(verbose_name="Data de Nascimento")
     telefone = models.CharField(max_length=11, verbose_name="Celular")
     endereco = models.CharField(max_length=255, verbose_name="Endereço")
 
     def __str__(self):
         return self.id_cliente
 
-'''
+
 class Locacao(models.Model):
 	id_locacao = 0 ##descobrir como gerar id auto incremento##
 	dataContratacao = models.DateField()
 	dataLocacao = models.DateField()
-    cliente = models.ForeignKey(Cliente, on_delete=models.PROTECT)
-   
-   def __str__(self):
+    #cliente = models.ForeignKey(Cliente, on_delete=models.PROTECT)
+def __str__(self):
         return self.id_locacao
 
 
 class ContaReceber(models.Model):
     id_recebimento = 0 ##descobrir como gerar id auto incremento##
-    id_locacao = models.ForeignKey(Locacao, on_delete=models.PROTECT)
-    id_cliente = models.ForeignKey(Cliente, on_delete=models.PROTECT)
+    #id_locacao = models.ForeignKey(Locacao, on_delete=models.PROTECT) Precisa criar locacao e client antes.
+    #id_cliente = models.ForeignKey(Cliente, on_delete=models.PROTECT)
     data_recebimento =  models.DateField()
-    valor_recebimento = models.DecimalField(decimal_places=2)  
+    valor_recebimento = models.DecimalField(max_digits = 8, decimal_places=2)  
 
     def __str__(self):
         return self.id_recebimento
-        '''
